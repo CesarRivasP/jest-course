4ec2e992c6d43d4e2add3fd29ab95b8b9c64ca4f
cesarrivas10 <cesarjose58@gmail.com>
2020-05-20 08:22:55 -0400

    Add new async test

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -31,4 +31,20 @@ test('Resuelve un Hola', async() => {
     await expect(Promise.resolve('Hola')).resolves.toBe('Hola')
     await expect(Promise.reject('Error')).rejects.toBe('Error')
   });
+
+test('Realizando una peticion a una API con error', async () => {
+    const API_ERROR = 'http://httpstat.us/404';
+    const response =  await getDataFromApi(API_ERROR);
+    // console.log(response);
+    
+    expect(response).rejects.toEqual(Error('Request failed with status code 404'));  //Error() equivale al tipo de error que retorno el server
+  });
+
+test('Realizando una peticion a una API con error', async () => {
+    const API_ERROR = 'http://httpstat.us/500';
+    const response =  await getDataFromApi(API_ERROR);
+    // console.log(response);
+    
+    expect(response).rejects.toEqual(Error('Request failed with status code 500'));  //Error() equivale al tipo de error que retorno el server
+  });
 });
